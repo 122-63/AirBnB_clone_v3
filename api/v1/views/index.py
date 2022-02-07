7afd149304dcb8be882730b457b26a5d1294c75a
122-63 <3469@holbertonschool.com>
2022-02-07 13:13:41 -0500

    Status of your API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', strict_slashes=False, methods=['GET'])
+@app_views.route('/status', strict_slashes=False, methods=['GET'] )
 def status():
     """function return status"""
-    return jsonify({'status': "OK"}), 200
+    return jsonify({'status': 'OK'})
