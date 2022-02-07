db5e3bf231da8952ff353717a26ef05079f6da2c
122-63 <3469@holbertonschool.com>
2022-02-07 16:15:37 -0500

    corretion parameters from index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,12 +3,12 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
-from models.amenity import Amenity
+"""from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
-from models.user import User
+from models.user import User"""
 
 
 @app_views.route('/status', strict_slashes=False, methods=['GET'])
@@ -18,7 +18,7 @@ def status():
 
 
 @app_views.route('/stats', strict_slashes=False, methods=['GET'])
-def count():
+def stats():
     """retrieves the number of each objects by type"""
     opctions = {
         "amenities": 47,
